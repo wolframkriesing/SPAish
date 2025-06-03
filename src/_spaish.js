ed41f3768f4f05ab50890b7f57138ceccb78ff96
Wolfram Kriesing <w+codeberg@kriesing.de>
2025-06-03 23:11:01 +0200

    A working version, putting the _spaish.js first in the build process, by _ prefix.

---
+++ src/_spaish.js
@@ -0,0 +1,2 @@
+// File is prefixed with "_" to come first in the build process.
+const spaish = {};
--- src/store.js
+++ src/store.js
@@ -1,47 +1,45 @@
-const spaish = {
-  sessionStore: {
-    _safeJsonParse: (value) => {
-      try {
-        return JSON.parse(value);
-      } catch {
-        return null;
-      }
-    },
-    writeArray: (sessionKey, array) => {
-      const items = JSON.stringify(array);
-      sessionStorage.setItem(sessionKey, items);
-    },
-    readArray: (sessionKey) => {
-      const values = sessionStorage.getItem(sessionKey);
-      const fromStorage = spaish.sessionStore._safeJsonParse(values);
-      return fromStorage === null ? [] : fromStorage;
-    },
-    /**
-     * @param sessionKey {string}
-     * @return {Map[string, any]}
-     */
-    readMap: (sessionKey) => {
-      const values = sessionStorage.getItem(sessionKey);
-      const fromStorage = spaish.sessionStore._safeJsonParse(values);
-      return fromStorage === null ? new Map() : new Map(fromStorage);
-    },
+spaish.sessionStore = {
+  _safeJsonParse: (value) => {
+    try {
+      return JSON.parse(value);
+    } catch {
+      return null;
+    }
+  },
+  writeArray: (sessionKey, array) => {
+    const items = JSON.stringify(array);
+    sessionStorage.setItem(sessionKey, items);
+  },
+  readArray: (sessionKey) => {
+    const values = sessionStorage.getItem(sessionKey);
+    const fromStorage = spaish.sessionStore._safeJsonParse(values);
+    return fromStorage === null ? [] : fromStorage;
+  },
+  /**
+   * @param sessionKey {string}
+   * @return {Map[string, any]}
+   */
+  readMap: (sessionKey) => {
+    const values = sessionStorage.getItem(sessionKey);
+    const fromStorage = spaish.sessionStore._safeJsonParse(values);
+    return fromStorage === null ? new Map() : new Map(fromStorage);
+  },
 
-    /**
-     * Write a Map to sessionStorage.
-     * @param sessionKey {string}
-     * @param map {Map[string, any]}
-     */
-    writeMap: (sessionKey, map) => {
-      const items = JSON.stringify([...map]);
-      sessionStorage.setItem(sessionKey, items);
-    },
+  /**
+   * Write a Map to sessionStorage.
+   * @param sessionKey {string}
+   * @param map {Map[string, any]}
+   */
+  writeMap: (sessionKey, map) => {
+    const items = JSON.stringify([...map]);
+    sessionStorage.setItem(sessionKey, items);
+  },
 
-    readString: (sessionKey) => {
-      const value = sessionStorage.getItem(sessionKey);
-      return value === null ? '' : value;
-    },
-    writeString: (sessionKey, value) => {
-      sessionStorage.setItem(sessionKey, value);
-    },
+  readString: (sessionKey) => {
+    const value = sessionStorage.getItem(sessionKey);
+    return value === null ? '' : value;
+  },
+  writeString: (sessionKey, value) => {
+    sessionStorage.setItem(sessionKey, value);
   },
 };
